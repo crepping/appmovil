dcc80180922cc03ffe7ccec57079cff6dd39ded5
Juan Cerna Escobar <juan.cerna@sfg.cl>
2020-11-28 18:34:52 -0300

    Guarda los archivos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 
 @NgModule({
+  
   imports: [
     CommonModule,
+    HttpClientModule,
     FormsModule,
     IonicModule,
     HomePageRoutingModule
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {NavController} from '@ionic/angular';
+import { HttpClient, HttpHeaders, HttpErrorResponse, } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  product: any = {}; 
+  constructor(public navCtrl: NavController, private HttpClient: HttpClient, public toast: ToastController) {}
+  ionViewDidLoad() {
+    console.log('Hello Addproduct Page'); 
+    }
 
-  constructor() {}
+ insert(){
+  this.product.action = "insert"; 
+  this.HttpClient.post('http://localhost/product.php', this.product).subscribe(data=>{
+  console.log(data); 
+    if (data['status']=='success'){
+      alert("Cliente Registrado");
+    }
+  }, err=>{
+  console.log(err); 
+  })
+  }
+
+  async showToast(message){
+  let toast = await this.toast.create({
+  message:message, 
+  duration: 2000
+  }); 
+  toast.present(); 
+  }
 
 }
